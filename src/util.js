33bf1d91bb0c698b903bf6d27d91ce954fa75a0c
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-12-05 12:34:26 +0530

    Modified parseInputs() and tests

--- src/util.js
+++ src/util.js
@@ -1,5 +1,11 @@
-const parseInputs = function(files){
-  let parsedInput = { option : '-n', numberOfLines : 10, files  : files.slice(0) };
+const parseInputs = function(files,option){
+  let parsedInput = { option : '-n', optionValue : 10, files  : files.slice(0) };
+
+  if(option){
+    option = option.join("");
+    parsedInput.option = option.slice(0,2);
+    parsedInput.optionValue = +option.slice(2);
+  }
   return parsedInput;
 }
 
--- test/utilTest.js
+++ test/utilTest.js
@@ -3,6 +3,6 @@ const { parseInputs } = require('../src/util.js');
 
 describe('parseInputs()', function(){
   it('should return a object where in files key the given files will be in array', function(){
-    deepEqual(parseInputs(['file1']),{option : '-n', numberOfLines : 10, files : ['file1'] });
+    deepEqual(parseInputs(['file1'],['-n4']),{option : '-n', optionValue : 4, files : ['file1'] });
   });
 });
