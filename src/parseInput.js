349ea6c20ee5fc658c035fc2addbc5534a4ee3c8
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-12-14 20:26:49 +0530

    Renamed tailIO.js to parseTnput.js which will be required both by head.js and tail.js

--- src/parseInput.js
+++ src/parseInput.js
@@ -6,23 +6,23 @@ const { isCase1ValidOptionCount,
         splitArgsForCase3
       } = require('../src/util.js');
 
-const parseInputs = function(tailArgs) {
-  let parsedInput = { option: "n", offset: 10, files: [...tailArgs] };
+const parseInputs = function(args) {
+  let parsedInput = { option: "n", offset: 10, files: [...args] };
 
   //case1 example : (a) node ./tail.js -n5 file1
   //                (b) node ./tail.js -n15 file1 file2
-  if (isCase1ValidOptionCount(tailArgs[0])) { 
-      parsedInput = splitArgsForCase1(tailArgs,parsedInput);
+  if (isCase1ValidOptionCount(args[0])) { 
+      parsedInput = splitArgsForCase1(args,parsedInput);
   }
   //case2 example : (a) node ./tail.js -n 5 file1
   //                (b) node ./tail.js -c 5 file1 file2  
-  if ( isCase2ValidOptionCount(tailArgs[0])) {
-     parsedInput = splitArgsForCase2(tailArgs,parsedInput);
+  if ( isCase2ValidOptionCount(args[0])) {
+     parsedInput = splitArgsForCase2(args,parsedInput);
   }
   //case3 example : (a) node ./tail.js -5 file1
   //                (b) node ./tail.js -10 file1 file2
-  if ( isCase3ValidOptionCount(tailArgs[0])) {
-     parsedInput = splitArgsForCase3(tailArgs,parsedInput);
+  if ( isCase3ValidOptionCount(args[0])) {
+     parsedInput = splitArgsForCase3(args,parsedInput);
   }
   return parsedInput;
 };
