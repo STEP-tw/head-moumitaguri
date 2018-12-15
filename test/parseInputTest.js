657112ed9434d71a25d4477fa718493e403eacfd
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-12-15 09:40:36 +0530

    Added parseInputTest.js file in test and tests for parsing input for headArgs
    node ./head.js -n5 file1
    node ./head.js -c5 file1
    node ./head.js -n5 file1 file2
    node ./head.js -c5 file1 file2
    node ./head.js -n 5 file1 file2
    node ./head.js -5 file1 file2

---
+++ test/parseInputTest.js
@@ -0,0 +1,35 @@
+const { equal, deepEqual } = require("assert");
+const { parseInputs } = require("../src/parseInput.js");
+
+describe("parseInputs() => should return an object having option : n ,count : 10,files : fileList,key-value-pairs", function () {
+  it('should work for input => headArgs = ["-n5","file"]', function () {
+    headArgs = ["-n5", "file"];
+    parsedInput = { option: "n", count: 5, files: ["file"] };
+    deepEqual(parseInputs(headArgs), parsedInput);
+  });
+  it('should work for input => headArgs = ["-c5","file"]', function () {
+    headArgs = ["-c5", "file"];
+    parsedInput = { option: "c", count: 5, files: ["file"] };
+    deepEqual(parseInputs(headArgs), parsedInput);
+  });
+  it('should work for input => headArgs = ["-n5","file","file2"]', function () {
+    headArgs = ["-n5", "file", "file2"];
+    parsedInput = { option: "n", count: 5, files: ["file", "file2"] };
+    deepEqual(parseInputs(headArgs), parsedInput);
+  });
+  it('should work for input => headArgs = ["-c5","file","file2"]', function () {
+    headArgs = ["-c5", "file", "file2"];
+    parsedInput = { option: "c", count: 5, files: ["file", "file2"] };
+    deepEqual(parseInputs(headArgs), parsedInput);
+  });
+  it('should work for input => headArgs = ["-n",5,"file","file2"]', function () {
+    headArgs = ["-n", "5", "file", "file2"];
+    parsedInput = { option: "n", count: 5, files: ["file", "file2"] };
+    deepEqual(parseInputs(headArgs), parsedInput);
+  });
+  it('should work for input => headArgs = ["-5","file","file2"]', function () {
+    headArgs = ["-5", "file", "file2"];
+    parsedInput = { option: "n", count: 5, files: ["file", "file2"] };
+    deepEqual(parseInputs(headArgs), parsedInput);
+  });
+});
