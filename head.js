8955ae91a111eb99e9faf8c63865ddd0e33eae79
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-12-15 14:19:09 +0530

    Invoked runCommand() instead head()

--- head.js
+++ head.js
@@ -14,7 +14,7 @@
   node ./head.js -c 5 file1 file2
   */
 
-const { head } = require("./src/lib.js");
+const { runCommand } = require("./src/lib.js");
 const { parseInputs } = require("./src/parseInput.js");
 const { readFileSync, existsSync } = require("fs");
 
@@ -22,7 +22,7 @@ const main = function () {
   let headArgs = process.argv.slice(2);
   let context = process.argv.slice(1)[0].substr(-7,4);
   let parsedInputs = parseInputs(headArgs);
-  console.log(head(existsSync, readFileSync, parsedInputs,context));
+  console.log(runCommand(existsSync, readFileSync, parsedInputs,context));
 };
 
 main();
