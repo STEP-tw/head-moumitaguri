6ffdfaa232cb58348713d1641674fb9567bca90c
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-12-18 15:15:09 +0530

    Used actualOut and expectedOut in tests for isHeadCountIllegal() in errorCheckTest.js

--- test/errorCheckTest.js
+++ test/errorCheckTest.js
@@ -6,44 +6,52 @@ const {
   isIllegalOffset
 } = require("../src/errorCheck.js");
 
-describe("isOptionIllegal", function() {
-  it("should return false when option is n", function() {
+describe("isOptionIllegal", function () {
+  it("should return false when option is n", function () {
     let actualOut = isOptionIllegal("n");
     let expectedOut = false;
     assert.deepEqual(actualOut, expectedOut);
   });
-  it("should return false when option is c", function() {
+  it("should return false when option is c", function () {
     let actualOut = isOptionIllegal("c");
     let expectedOut = false;
     assert.deepEqual(actualOut, expectedOut);
   });
-  it("should return true when option is neither n nor c", function() {
+  it("should return true when option is neither n nor c", function () {
     let actualOut = isOptionIllegal("t");
     let expectedOut = true;
     assert.deepEqual(actualOut, expectedOut);
   });
 });
 
-describe("isHeadCountIllegal()", function() {
-  it("should return true when count is 0", function() {
-    assert.deepEqual(isHeadCountIllegal(0), true);
+describe("isHeadCountIllegal", function () {
+  it("should return true when count is 0", function () {
+    let actualOut = isHeadCountIllegal(0);
+    let expectedOut = true;
+    assert.deepEqual(actualOut, expectedOut);
   });
-  it("should return true when count is negative", function() {
-    assert.deepEqual(isHeadCountIllegal(-1), true);
+  it("should return true when count is negative", function () {
+    let actualOut = isHeadCountIllegal(-1);
+    let expectedOut = true;
+    assert.deepEqual(actualOut, expectedOut);
   });
-  it("should return true when count is NaN", function() {
-    assert.deepEqual(isHeadCountIllegal("n"), true);
+  it("should return true when count is NaN", function () {
+    let actualOut = isHeadCountIllegal("n");
+    let expectedOut = true;
+    assert.deepEqual(actualOut, expectedOut);
   });
-  it("should return false when count is > 1", function() {
-    assert.deepEqual(isHeadCountIllegal(2), false);
+  it("should return false when count is > 1", function () {
+    let actualOut = isHeadCountIllegal(2);
+    let expectedOut = false;
+    assert.deepEqual(actualOut, expectedOut);
   });
 });
 
-describe("isIllegalOffset()", function() {
-  it("should return false when offset is 0", function() {
+describe("isIllegalOffset()", function () {
+  it("should return false when offset is 0", function () {
     assert.deepEqual(isIllegalOffset(0), false);
   });
-  it("should return true when offset is NaN", function() {
+  it("should return true when offset is NaN", function () {
     assert.deepEqual(isIllegalOffset("n"), true);
   });
 });
