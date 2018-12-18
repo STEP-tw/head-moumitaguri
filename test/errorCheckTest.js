a3168a9ad5c649ad05292ae0a56f7f58f86021c2
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-12-18 12:33:54 +0530

    Removed unnecessary tests from errorCheckTest.js

--- test/errorCheckTest.js
+++ test/errorCheckTest.js
@@ -2,11 +2,7 @@ const assert = require('assert');
 
 const { isOptionIllegal,
   isHeadCountIllegal,
-  isIllegalOffset,
-  printHeadIllegalCountError,
-  printHeadIllegalOptionUsageErrorMessage,
-  printTailIllegalOffsetError,
-  printTailIllegalOptionUsageError,
+  isIllegalOffset
 } = require('../src/errorCheck.js');
 
 
@@ -44,33 +40,5 @@ describe('isIllegalOffset()', function () {
   });
   it('should return true when offset is NaN', function () {
     assert.deepEqual(isIllegalOffset("n"), true);
-  });
-});
-
-describe('printHeadIllegalCountError()', function () {
-  it('should return string showing illegal line count error ', function () {
-    assert.deepEqual(printHeadIllegalCountError(0, "n"), "head: illegal line count -- 0");
-  });
-  it('should return string showing illegal byte count error ', function () {
-    assert.deepEqual(printHeadIllegalCountError(0, "c"), "head: illegal byte count -- 0");
-  });
-});
-
-describe('printHeadIllegalOptionUsageErrorMessage()', function () {
-  it('should return string showing illegal option error', function () {
-    assert.deepEqual(printHeadIllegalOptionUsageErrorMessage("v"), "head: illegal option -- v\nusage: head [-n lines | -c bytes] [file ...]");
-  });
-});
-
-
-describe('printTailIllegalOffsetError()', function () {
-  it('should return string showing illegal offset NaN ', function () {
-    assert.deepEqual(printTailIllegalOffsetError("c"), "tail: illegal offset -- c");
-  });
-});
-
-describe('printTailIllegalOptionUsageError()', function () {
-  it('should return string showing illegal option error', function () {
-    assert.deepEqual(printTailIllegalOptionUsageError("v"), "tail: illegal option -- v\nusage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]");
   });
 });
