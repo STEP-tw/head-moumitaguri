4880151e1d82d681760d16658a33fd71ec8b6f64
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-12-18 14:49:44 +0530

    Removed unnecessary commented lines from input.js

--- src/input.js
+++ src/input.js
@@ -2,14 +2,11 @@ const { isOption, isNumber, isDash
 } = require('./util.js');
 
 
-//case1 example : (a) node ./tail.js -n5 file1
-//                (b) node ./tail.js -n15 file1 file2 
 const areOptionCountTogether = function (args) {
    return args.length >= 3 &&
       isDash(args[0]) &&
       isOption(args[1]);
 }
-
 
 const splitWhenOptionCountTogether = function (args, parsedInput) {
    parsedInput.count = args[0].slice(2);
@@ -18,8 +15,6 @@ const splitWhenOptionCountTogether = function (args, parsedInput) {
    return parsedInput;
 }
 
-//case2 example : (a) node ./tail.js -n 5 file1
-//                (b) node ./tail.js -c 5 file1 file2
 const areOptionCountSeparated = function (args) {
    return args.length == 2 &&
       isDash(args[0]) &&
@@ -33,9 +28,6 @@ const splitWhenOptionCountSeparated = function (args, parsedInput) {
    return parsedInput;
 }
 
-
-//case3 example : (a) node ./tail.js -5 file1
-//                (b) node ./tail.js -10 file1 file2
 const isOnlyCountGiven = function (args) {
    return args.length >= 2 &&
       isNumber(args.slice(1));
@@ -51,18 +43,12 @@ const splitWhenOnlyCountGiven = function (args, parsedInput) {
 const parseInputs = function (args) {
    let parsedInput = { option: "n", count: 10, files: [...args] };
 
-   //case1 example : (a) node ./tail.js -n5 file1
-   //                (b) node ./tail.js -n15 file1 file2
    if (areOptionCountTogether(args[0])) {
       parsedInput = splitWhenOptionCountTogether(args, parsedInput);
    }
-   //case2 example : (a) node ./tail.js -n 5 file1
-   //                (b) node ./tail.js -c 5 file1 file2  
    if (areOptionCountSeparated(args[0])) {
       parsedInput = splitWhenOptionCountSeparated(args, parsedInput);
    }
-   //case3 example : (a) node ./tail.js -5 file1
-   //                (b) node ./tail.js -10 file1 file2
    if (isOnlyCountGiven(args[0])) {
       parsedInput = splitWhenOnlyCountGiven(args, parsedInput);
    }
